ddbe0b8a8440fcd051db2a5430f9dd0e5d790d06
chory <chory@192.168.0.27>
2026-02-22 18:03:11 -0800

    Completed assignment

--- src/DodgeBall.java
+++ src/DodgeBall.java
@@ -3,9 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
-
 import javax.swing.Timer;
-
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import acm.graphics.GOval;
@@ -16,80 +14,143 @@ import acm.util.RandomGenerator;
 public class DodgeBall extends GraphicsProgram implements ActionListener {
 	private ArrayList<GOval> balls;
 	private ArrayList<GRect> enemies;
-	private GLabel text;
+	private GLabel enemyCountLabel;
+	private GLabel destroyedLabel;
+	private GLabel survivalLabel;
 	private Timer movement;
 	private RandomGenerator rgen;
-	
+
 	public static final int SIZE = 25;
 	public static final int SPEED = 2;
 	public static final int MS = 50;
 	public static final int MAX_ENEMIES = 10;
 	public static final int WINDOW_HEIGHT = 600;
 	public static final int WINDOW_WIDTH = 300;
-	
+
+	private int numTimes = 0;
+	private int destroyed = 0;
+
+	public void init() {
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	}
+
 	public void run() {
 		rgen = RandomGenerator.getInstance();
 		balls = new ArrayList<GOval>();
 		enemies = new ArrayList<GRect>();
-		
-		text = new GLabel(""+enemies.size(), 0, WINDOW_HEIGHT);
-		add(text);
-		
+
+		enemyCountLabel = new GLabel("Enemies: 0", 0, WINDOW_HEIGHT - 30);
+		add(enemyCountLabel);
+
+		destroyedLabel = new GLabel("Destroyed: 0", 0, WINDOW_HEIGHT - 15);
+		add(destroyedLabel);
+
+		survivalLabel = new GLabel("Survival time: 0", 80, WINDOW_HEIGHT - 15);
+		add(survivalLabel);
+
 		movement = new Timer(MS, this);
 		movement.start();
 		addMouseListeners();
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+
+		// Update survival time label
+		survivalLabel.setLabel("Survival time: " + numTimes);
+
+		// Add enemy every 40 ticks
+		if (numTimes % 40 == 0) {
+			addAnEnemy();
+		}
+
 		moveAllBallsOnce();
+		moveAllEnemiesOnce();
+		checkCollisions();
+
+		// Check if MAX_ENEMIES exceeded
+		if (enemies.size() > MAX_ENEMIES) {
+			movement.stop();
+			removeAll();
+			GLabel lostLabel = new GLabel("You lost! Survival time: " + numTimes, 20, WINDOW_HEIGHT / 2);
+			add(lostLabel);
+		}
 	}
-	
+
 	public void mousePressed(MouseEvent e) {
-		for(GOval b:balls) {
-			if(b.getX() < SIZE * 2.5) {
+		for (GOval b : balls) {
+			if (b.getX() < SIZE * 2.5) {
 				return;
 			}
 		}
-		addABall(e.getY());     
+		addABall(e.getY());
 	}
-	
+
 	private void addABall(double y) {
-		GOval ball = makeBall(SIZE/2, y);
+		GOval ball = makeBall(SIZE / 2, y);
 		add(ball);
 		balls.add(ball);
 	}
-	
+
 	public GOval makeBall(double x, double y) {
-		GOval temp = new GOval(x-SIZE/2, y-SIZE/2, SIZE, SIZE);
+		GOval temp = new GOval(x - SIZE / 2, y - SIZE / 2, SIZE, SIZE);
 		temp.setColor(Color.RED);
 		temp.setFilled(true);
 		return temp;
 	}
-	
+
 	private void addAnEnemy() {
-		GRect e = makeEnemy(rgen.nextInt(0, WINDOW_HEIGHT-SIZE/2));
+		GRect e = makeEnemy(rgen.nextInt(0, WINDOW_HEIGHT - SIZE / 2));
 		enemies.add(e);
-		text.setLabel("" + enemies.size());
+		enemyCountLabel.setLabel("Enemies: " + enemies.size());
 		add(e);
 	}
-	
+
 	public GRect makeEnemy(double y) {
-		GRect temp = new GRect(WINDOW_WIDTH-SIZE, y-SIZE/2, SIZE, SIZE);
+		GRect temp = new GRect(WINDOW_WIDTH - SIZE, y - SIZE / 2, SIZE, SIZE);
 		temp.setColor(Color.GREEN);
 		temp.setFilled(true);
 		return temp;
 	}
 
 	private void moveAllBallsOnce() {
-		for(GOval ball:balls) {
+		for (GOval ball : balls) {
 			ball.move(SPEED, 0);
 		}
 	}
-	
-	public void init() {
-		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	private void moveAllEnemiesOnce() {
+		for (GRect enemy : enemies) {
+			enemy.move(0, rgen.nextInt(-2, 2));
+		}
 	}
-	
+
+	private void checkCollisions() {
+		ArrayList<GRect> toRemove = new ArrayList<GRect>();
+
+		for (GOval ball : balls) {
+			// Check the point just in front of the ball
+			double checkX = ball.getX() + ball.getWidth() + 1;
+			double checkY = ball.getY() + ball.getHeight() / 2;
+			GObject obj = getElementAt(checkX, checkY);
+
+			if (obj instanceof GRect) {
+				GRect hit = (GRect) obj;
+				if (!toRemove.contains(hit)) {
+					toRemove.add(hit);
+				}
+			}
+		}
+
+		for (GRect enemy : toRemove) {
+			remove(enemy);
+			enemies.remove(enemy);
+			destroyed++;
+			destroyedLabel.setLabel("Destroyed: " + destroyed);
+			enemyCountLabel.setLabel("Enemies: " + enemies.size());
+		}
+	}
+
 	public static void main(String args[]) {
 		new DodgeBall().start();
 	}
--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,22 +1,42 @@
 import acm.graphics.*;
 import acm.program.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private int numTimes;
+	private Timer t;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
 		requestFocus();
 	}
-	
+
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+
+		numTimes = 0;
+
+		t = new Timer(1000, this);
+		t.setInitialDelay(3000); // 3 second delay before starting
+		t.start();
 	}
-	
+
+	public void actionPerformed(ActionEvent e) {
+		myLabel.move(5, 0);
+		numTimes++;
+		myLabel.setLabel("times called? " + numTimes);
+
+		if (numTimes == 10) {
+			t.stop();
+		}
+	}
+
 	public static void main(String[] args) {
 		new MyFirstTimer().start();
 	}
